451d523daee78cf433f672ca6b9895164bee3c28
Blueeeeeeeeeeeeee <142000749+Blueeeeeeeeeeeeee@users.noreply.github.com>
2023-08-15 18:21:23 +0800

    Adding PlayerMovement script

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody2D rb2d;
+    Collider2D col2d;
+    Camera mainCam;
+    bool isDragged = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+        col2d = GetComponent<Collider2D>();
+        mainCam = Camera.main;
     }
 
+    private void OnMouseDown()
+    {
+        isDragged = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isDragged = false;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (isDragged)
+        {
+            Vector2 mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+            rb2d.MovePosition(mousePos);
+        }
     }
 }
