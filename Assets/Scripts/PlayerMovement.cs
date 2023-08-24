bff5cb4e61ab16edbcaa25e31a7dc06cb2ef4892
Blueeeeeeeeeeeeee <142000749+Blueeeeeeeeeeeeee@users.noreply.github.com>
2023-08-24 18:10:46 +0800

    PlayerMovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    Collider2D col2d;
+    SpriteRenderer sRenderer;
+    Camera mainCam;
+    bool isDragged = false;
+
+    [SerializeField] private SpriteRenderer bg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        col2d = GetComponent<Collider2D>();
+        sRenderer = GetComponent<SpriteRenderer>();
+        mainCam = Camera.main;
+    }
+
+
+
+
+
+    void OnMouseDown()
+    {
+        isDragged = true;
+    }
+
+    void OnMouseUp()
+    {
+        isDragged = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDragged)
+        {
+            
+            Vector2 mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.x = Mathf.Clamp(mousePos.x, bg.transform.position.x - (bg.bounds.size.x / 2) + (sRenderer.bounds.size.x),
+                bg.transform.position.x + (bg.bounds.size.x/2)) - (sRenderer.bounds.size.x / 2);
+            mousePos.y = Mathf.Clamp(mousePos.y, bg.transform.position.y - (bg.bounds.size.y / 2) + (sRenderer.bounds.size.y),
+                bg.transform.position.y + (bg.bounds.size.y/2)) - (sRenderer.bounds.size.y / 2);
+            /*Debug.Log("Mouse Pos" + mousePos);
+            Debug.Log("Transform Pos" + bg.transform.position);
+            Debug.Log("Bound size" + bg.bounds.size);*/
+            rb2d.MovePosition(mousePos);
+        }
+    }
+}
